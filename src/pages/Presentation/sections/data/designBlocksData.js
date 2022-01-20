a23357144a805cdf0d372af6d7909690033e745e
gopi4753 <gopi850193@gmail.com>
2022-01-20 12:06:44 +0530

    first commit

---
+++ src/pages/Presentation/sections/data/designBlocksData.js
@@ -0,0 +1,260 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+const imagesPrefix =
+  "https://raw.githubusercontent.com/creativetimofficial/public-assets/master/material-design-system/presentation/sections";
+
+export default [
+  {
+    title: "Popular Cakes",
+    description: "A selection of 45 page sections that fit perfectly in any combination",
+    items: [
+      {
+        image: `https://giftzbag.com/wp-content/uploads/2021/04/Ferrero-Pull-me-Up-Cake.jpg`,
+        name: "Page Headers",
+        count: 10,
+        route: "/sections/page-sections/page-headers",
+      },
+      {
+        image: `https://media.bakingo.com/chocolaty-ecstasy-hidden-in-a-half-kg-cake-bomb-bomb2397choc-B.jpg`,
+        name: "Features",
+        count: 14,
+        route: "/sections/page-sections/features",
+      },
+      {
+        image: `https://media.bakingo.com/round-shaped-vanilla-cake-4-cake904vani-D.jpg`,
+        name: "Pricing",
+        count: 14,
+        route: "/sections/page-sections/features",
+      },
+      {
+        image: `https://5.imimg.com/data5/NL/EO/MY-41386623/chocolate-truffle-cake-500x500.png`,
+        name: "FAQ",
+        count: 1,
+        route: "/sections/page-sections/features",
+      },
+      {
+        image: `https://imgcdn.floweraura.com/Butterscotch-Round-Shape-Cake-03-A.jpg`,
+        name: "Blog Posts",
+        count: 11,
+        route: "/sections/page-sections/features",
+      },
+      {
+        image: `https://5.imimg.com/data5/NL/EO/MY-41386623/chocolate-truffle-cake.png`,
+        name: "Testimonials",
+        count: 11,
+        route: "/sections/page-sections/features",
+      },
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR34w0Oe9gm8ZHwUk_v5unUwJImnup-Ka6i_A&usqp=CAU`,
+        name: "Teams",
+        count: 6,
+        route: "/sections/page-sections/features",
+      },
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBKY51s-IMqGsuPiuycwQR6zccDgpXEuWZEw&usqp=CAU`,
+        name: "Stats",
+        count: 3,
+        route: "/sections/page-sections/features",
+      },
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHKiwu34uWBoDcwKGDFzm4GsyDpI5rPV1ORg&usqp=CAU`,
+        name: "Call to Actions",
+        count: 8,
+        route: "/sections/page-sections/features",
+      },
+      {
+        image: `${imagesPrefix}/projects.jpg`,
+        name: "Applications",
+        count: 6,
+        pro: true,
+      },
+      {
+        image: `${imagesPrefix}/logo-area.jpg`,
+        name: "Logo Areas",
+        count: 4,
+        pro: true,
+      },
+      {
+        image: `${imagesPrefix}/footers.jpg`,
+        name: "Footers",
+        count: 10,
+        pro: true,
+      },
+      {
+        image: `${imagesPrefix}/general-cards.jpg`,
+        name: "General Cards",
+        count: 9,
+        pro: true,
+      },
+      {
+        image: `${imagesPrefix}/content-sections.jpg`,
+        name: "Content Sections",
+        count: 8,
+        pro: true,
+      },
+    ],
+  },
+  {
+    title: "Photo Cakes",
+    description: "30+ components that will help go through the pages",
+    items: [
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1QOjKyjLMxs6-kl6-3swfSapai-68ebWC7g&usqp=CAU`,
+        name: "Navbars",
+        count: 4,
+        route: "/sections/navigation/navbars",
+      },
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy0Wr1rO94WFQDytaI22-xRZfC6bBnzNWBHw&usqp=CAU`,
+        name: "Nav Tabs",
+        count: 2,
+        route: "/sections/navigation/nav-tabs",
+      },
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtldxormoM8DMF7-lpGTu0m1g1pwhZY-zOGw&usqp=CAU`,
+        name: "Pagination",
+        count: 3,
+        route: "/sections/navigation/pagination",
+      },
+    ],
+  },
+  {
+    title: "Designer Cakes",
+    description: "50+ elements that you need for text manipulation and insertion",
+    items: [
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8QVCVof3DjrbV9Av65cOokpnFA2VB_hmUCg&usqp=CAU`,
+        name: "Newsletters",
+        count: 6,
+        pro: true,
+      },
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4tdnd2mCCMGM2JJar_ziMlps46HuDQsqPqQ&usqp=CAU`,
+        name: "Contact Sections",
+        count: 8,
+        pro: true,
+      },
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvse9l-MOH_ys_rNje_Mtjd3zCS6-xCGAp_Q&usqp=CAU`,
+        name: "Forms",
+        count: 3,
+        route: "/sections/input-areas/forms",
+      },
+      {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHbINb8NuAcyaAbWUbhohh2E284D3YT0xCUw&usqp=CAU`,
+        name: "Inputs",
+        count: 6,
+        route: "/sections/input-areas/inputs",
+      },
+    ],
+  },
+  {
+    title: "Attention Catchers",
+    description: "20+ Fully coded components that popup from different places of the screen",
+    items: [
+      {
+        image: `${imagesPrefix}/alerts.jpg`,
+        name: "Alerts",
+        count: 4,
+        route: "/sections/attention-catchers/alerts",
+      },
+      {
+        image: `${imagesPrefix}/toasts.jpg`,
+        name: "Notifications",
+        count: 3,
+        pro: true,
+      },
+      {
+        image: `${imagesPrefix}/popovers.jpg`,
+        name: "Tooltips & Popovers",
+        count: 2,
+        route: "/sections/attention-catchers/tooltips-popovers",
+      },
+      {
+        image: `${imagesPrefix}/modals.jpg`,
+        name: "Modals",
+        count: 5,
+        route: "/sections/attention-catchers/modals",
+      },
+    ],
+  },
+  {
+    title: "Elements",
+    description: "80+ carefully crafted small elements that come with multiple colors and shapes",
+    items: [
+      {
+        image: `${imagesPrefix}/buttons.jpg`,
+        name: "Buttons",
+        count: 6,
+        route: "/sections/elements/buttons",
+      },
+      {
+        image: `${imagesPrefix}/avatars.jpg`,
+        name: "Avatars",
+        count: 2,
+        route: "/sections/elements/avatars",
+      },
+      {
+        image: `${imagesPrefix}/dropdowns.jpg`,
+        name: "Dropdowns",
+        count: 2,
+        route: "/sections/elements/dropdowns",
+      },
+      {
+        image: `${imagesPrefix}/switch.jpg`,
+        name: "Toggles",
+        count: 2,
+        route: "/sections/elements/toggles",
+      },
+      {
+        image: `${imagesPrefix}/social-buttons.jpg`,
+        name: "Social Buttons",
+        count: 2,
+        pro: true,
+      },
+      {
+        image: `${imagesPrefix}/breadcrumbs.jpg`,
+        name: "Breadcrumbs",
+        count: 1,
+        route: "/sections/elements/breadcrumbs",
+      },
+      {
+        image: `${imagesPrefix}/badges.jpg`,
+        name: "Badges",
+        count: 3,
+        route: "/sections/elements/badges",
+      },
+      {
+        image: `${imagesPrefix}/progress.jpg`,
+        name: "Progress Bars",
+        count: 4,
+        route: "/sections/elements/progress-bars",
+      },
+      {
+        image: `${imagesPrefix}/tables.jpg`,
+        name: "Tables",
+        count: 3,
+        pro: true,
+      },
+      {
+        image: `${imagesPrefix}/typography.jpg`,
+        name: "Typography",
+        count: 2,
+        route: "/sections/elements/typography",
+      },
+    ],
+  },
+];
